c928f533ea2fc0150252e3e59421f2bd4fa97fc2
YaniYakimov <yani.v.yakimov@gmail.com>
2023-03-22 15:13:01 +0200

    adding all words in Map first, then writing to dif files

--- src/new_files/Demo.java
+++ src/new_files/Demo.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.Scanner;
+import java.util.*;
 
 public class Demo {
     public static void main(String[] args) throws IOException {
@@ -13,24 +13,26 @@ public class Demo {
         String warPeace = Files.readString(Path.of(f1.getPath()));
         Scanner sc = new Scanner(warPeace);
         long startTime = System.currentTimeMillis();
+        Map<Integer, Set> words = new HashMap<>();
         while(sc.hasNextLine()) {
             String line = sc.nextLine();
-            String[] words = line.split(" ");
-            for (int i = 0; i < words.length; i++) {
-                File f = new File("_.txt");
-                if(!f.exists()) {
-                    f = new File(words[i].length() + ".txt");
-                    f.createNewFile();
+            String[] allWords = line.split(" ");
+            for (int i = 0; i < allWords.length; i++) {
+                if(!words.containsKey(allWords[i].length())) {
+                    words.put(allWords[i].length(), new HashSet<String>());
                 }
-
-                if(f.getName().equals(words[i].length() + ".txt")) {
-                    Files.write(f.toPath(), (words[i] + " ").getBytes(), StandardOpenOption.APPEND);
-                }
-                else{
-                    Files.write(f.toPath(), words[i].getBytes());
-                }
+                words.get(allWords[i].length()).add(allWords[i]);
             }
-            System.out.println();
+        }
+        for(Map.Entry<Integer, Set> entry : words.entrySet()) {
+            File f = new File("_.txt");
+            if(!f.exists()) {
+                f = new File(entry.getKey() + ".txt");
+                f.createNewFile();
+            }
+            for(Object string : entry.getValue()) {
+                Files.write(f.toPath(), (string + " ").getBytes(), StandardOpenOption.APPEND);
+            }
         }
         System.out.println(System.currentTimeMillis() - startTime);
     }
